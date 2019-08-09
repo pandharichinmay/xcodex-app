366724cd22d9daa7a5d2c7d9e369dbb72a268334
Chinmay <pandharichinmay@gmail.com>
2019-08-09 13:38:37 +0530

    Commit CodeX

--- app/src/main/java/com/example/codex/AddOrder.java
+++ app/src/main/java/com/example/codex/AddOrder.java
@@ -1,13 +1,141 @@
 package com.example.codex;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.codex.model.bo.CustomerMaster;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import static android.R.layout.simple_spinner_item;
 
 public class AddOrder extends AppCompatActivity {
+    private String URLstring = "http://192.168.1.25:8080/getAllCustomer";
+    private static ProgressDialog mProgressDialog;
+    private ArrayList<CustomerMaster> goodModelArrayList;
+    private ArrayList<String> names = new ArrayList<String>();
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_order);
+        spinner = findViewById(R.id.spinnerCustomer);
+
+        retrieveJSON();
+
+    }
+
+    private void retrieveJSON() {
+        showSimpleProgressDialog(this, "Loading...", "Fetching Json", false);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URLstring,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        Log.d("strrrrr", ">>" + response);
+
+                        try {
+
+                            JSONObject obj = new JSONObject(response);
+                            if (obj.optString("status").equals("true")) {
+
+                                goodModelArrayList = new ArrayList<>();
+                                JSONArray dataArray = obj.getJSONArray("data");
+
+                                for (int i = 0; i < dataArray.length(); i++) {
+
+                                    CustomerMaster playerModel = new CustomerMaster();
+                                    JSONObject dataobj = dataArray.getJSONObject(i);
+
+                                    playerModel.setCustName(dataobj.getString("name"));
+                                    goodModelArrayList.add(playerModel);
+
+                                }
+
+                                for (int i = 0; i < goodModelArrayList.size(); i++) {
+                                    names.add(goodModelArrayList.get(i).getCustName().toString());
+                                }
+
+                                ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(AddOrder.this, simple_spinner_item, names);
+                                spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+                                spinner.setAdapter(spinnerArrayAdapter);
+                                removeSimpleProgressDialog();
+
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //displaying the error in toast if occurrs
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // request queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        requestQueue.add(stringRequest);
+    }
+
+    public static void removeSimpleProgressDialog() {
+        try {
+            if (mProgressDialog != null) {
+                if (mProgressDialog.isShowing()) {
+                    mProgressDialog.dismiss();
+                    mProgressDialog = null;
+                }
+            }
+        } catch (IllegalArgumentException ie) {
+            ie.printStackTrace();
+
+        } catch (RuntimeException re) {
+            re.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showSimpleProgressDialog(Context context, String title,
+                                          String msg, boolean isCancelable) {
+        try {
+            if (mProgressDialog == null) {
+                mProgressDialog = ProgressDialog.show(context, title, msg);
+                mProgressDialog.setCancelable(isCancelable);
+            }
+
+            if (!mProgressDialog.isShowing()) {
+                mProgressDialog.show();
+            }
+
+        } catch (IllegalArgumentException ie) {
+            ie.printStackTrace();
+        } catch (RuntimeException re) {
+            re.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/codex/OrderDetails.java
+++ app/src/main/java/com/example/codex/OrderDetails.java
@@ -23,119 +23,16 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-import static android.R.layout.simple_spinner_item;
 public class OrderDetails extends AppCompatActivity {
-    private String URLstring = "http://192.168.1.25:8080/getAllCustomer";
-    private static ProgressDialog mProgressDialog;
-    private ArrayList<CustomerMaster> goodModelArrayList;
-    private ArrayList<String> names = new ArrayList<String>();
-    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_details);
-        spinner = findViewById(R.id.spinnerCustomer);
 
-        retrieveJSON();
 
     }
 
-    private void retrieveJSON() {
-        showSimpleProgressDialog(this, "Loading...", "Fetching Json", false);
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, URLstring,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-
-                        Log.d("strrrrr", ">>" + response);
-
-                        try {
-
-                            JSONObject obj = new JSONObject(response);
-                            if (obj.optString("status").equals("true")) {
-
-                                goodModelArrayList = new ArrayList<>();
-                                JSONArray dataArray = obj.getJSONArray("data");
-
-                                for (int i = 0; i < dataArray.length(); i++) {
-
-                                    CustomerMaster playerModel = new CustomerMaster();
-                                    JSONObject dataobj = dataArray.getJSONObject(i);
-
-                                    playerModel.setCustName(dataobj.getString("name"));
-                                    goodModelArrayList.add(playerModel);
-
-                                }
-
-                                for (int i = 0; i < goodModelArrayList.size(); i++) {
-                                    names.add(goodModelArrayList.get(i).getCustName().toString());
-                                }
-
-                                ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(OrderDetails.this, simple_spinner_item, names);
-                                spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
-                                spinner.setAdapter(spinnerArrayAdapter);
-                                removeSimpleProgressDialog();
-
-                            }
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        //displaying the error in toast if occurrs
-                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-        // request queue
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-
-        requestQueue.add(stringRequest);
-    }
-
-    public static void removeSimpleProgressDialog() {
-        try {
-            if (mProgressDialog != null) {
-                if (mProgressDialog.isShowing()) {
-                    mProgressDialog.dismiss();
-                    mProgressDialog = null;
-                }
-            }
-        } catch (IllegalArgumentException ie) {
-            ie.printStackTrace();
-
-        } catch (RuntimeException re) {
-            re.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void showSimpleProgressDialog(Context context, String title,
-                                          String msg, boolean isCancelable) {
-        try {
-            if (mProgressDialog == null) {
-                mProgressDialog = ProgressDialog.show(context, title, msg);
-                mProgressDialog.setCancelable(isCancelable);
-            }
-
-            if (!mProgressDialog.isShowing()) {
-                mProgressDialog.show();
-            }
-
-        } catch (IllegalArgumentException ie) {
-            ie.printStackTrace();
-        } catch (RuntimeException re) {
-            re.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
 
